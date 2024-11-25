c488206a91d55b03cae1c456d703a6a14b16aa5d
Prem Dharshan <whizzkid.dpd@gmail.com>
2024-11-25 15:32:38 +0530

    Create schema.sql

---
+++ src/main/resources/db_scripts/schema.sql
@@ -0,0 +1,20 @@
+-- Create the database if it does not already exist
+CREATE DATABASE IF NOT EXISTS sb_boilerplate;
+
+-- Use the newly created or existing database
+USE sb_boilerplate;
+
+-- Create the table for storing organization data
+CREATE TABLE IF NOT EXISTS t_organization (
+                                              id INT AUTO_INCREMENT PRIMARY KEY,
+                                              name VARCHAR(64) NOT NULL,
+                                              description TEXT
+
+);
+
+-- Grant privileges to the specified user
+-- Replace 'root' and 'psg@123' with your actual username and password
+GRANT ALL PRIVILEGES ON sb_boilerplate.* TO 'root'@'localhost' IDENTIFIED BY 'psg@123';
+
+-- Apply the privileges
+FLUSH PRIVILEGES;
