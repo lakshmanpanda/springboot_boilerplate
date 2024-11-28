f21c604c284ab6ac392dd2d4998373ed966d5559
Prem Dharshan <whizzkid.dpd@gmail.com>
2024-11-28 13:05:49 +0530

    Resolve CORS

--- src/main/java/com/sample/springboot_boilerplate/controller/UserController.java
+++ src/main/java/com/sample/springboot_boilerplate/controller/UserController.java
@@ -6,11 +6,13 @@ import com.sample.springboot_boilerplate.exception.ResourceNotFoundException;
 import com.sample.springboot_boilerplate.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
     private final UserService userService;
